ac661406b4c8e9d33db3b59e3fd16be1b2b21e9d
V <maja5225@hotmail.com>
2020-10-15 11:20:36 +0200

    Edits style of navbar, footer and font colors

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+}
 
-  ngOnInit(): void {
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const navSelector = document.querySelector('nav');
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    navSelector.style.backgroundColor = '#035959';
+    navSelector.style.backgroundImage = 'unset';
+    navSelector.style.padding = '10px 10px';
+    // document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    navSelector.style.backgroundColor = 'unset';
+    navSelector.style.backgroundImage =
+      'linear-gradient(rgba(3, 89, 89, 0.8), rgba(0, 0, 0, 0))';
+    navSelector.style.padding = '30px 10px';
+    // document.getElementById('logo').style.fontSize = '35px';
   }
-
 }
